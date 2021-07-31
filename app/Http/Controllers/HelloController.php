d3c9cc304362c7053a44d81965593eb639722257
Takaya Seiwa <ljks1021@gmail.com>
2021-07-31 17:25:48 +0900

    データベースからのデータの取得

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
+use Illuminate\Support\Facades\DB;
+
+class HelloController extends Controller
+{
+    public function index(Request $request)
+    {
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
+    }
+
+    public function post(HelloRequest $request)
+    {
+        return view('hello.index', ['msg' => '正しく入力されました！']);
+    }
+}
---
+++ app/Http/Requests/HelloRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class HelloRequest extends FormRequest
+{
+    public function authorize()
+    {
+        if ($this->path() == 'hello') {
+            return true;
+        }else {
+            return false;            
+        }
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '名前は必ず入力して下さい。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢は整数で記入下さい',
+            'age.between' => '年齢は0~150の間で入力下さい',
+        ];
+    }
+}
