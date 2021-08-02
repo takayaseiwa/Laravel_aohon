a3c315bf44c736bf3a8edc384856f87087468cb3
Takaya Seiwa <ljks1021@gmail.com>
2021-08-02 22:05:49 +0900

    マイグレーションの実装

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,7 +11,7 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::table('people')->get();
+        $items = DB::table('people')->orderBy('age', 'asc')->get();
         return view('hello.index',['items' => $items]);
     }
 
@@ -31,25 +31,26 @@ class HelloController extends Controller
             'mail' => $request->mail,
             'age' => $request->age,
         ];
-        DB::insert('insert into people (name, mail, age) values
-              (:name, :mail, :age)', $param);
+        DB::table('people')->insert($param);
         return redirect('/hello');
     }
 
     public function edit(Request $request){
-        $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id',$param);
-        return view('hello.edit', ['form' => $item[0]]);
+        $item = DB::table('people')
+        ->where('id', $request->id)
+        ->first();
+        return view('hello.edit', ['form' => $item]);
     }
 
     public function update(Request $request){
         $param = [
-            'id' => $request->id,
             'name' => $request->name,
             'mail' => $request->mail,
             'age' => $request->age,
         ];
-        DB::update('update people set name =:name, mail=:mail, age=:age where id =:id', $param);
+        DB::table('people')
+        ->where('id', $request->id)
+        ->update($param);
         return redirect('/hello');
     }
 
@@ -66,8 +67,11 @@ class HelloController extends Controller
     }
 
     public function show(Request $request){
-        $id = $request->id;
-        $item = DB::table('people')->where('id', $id)->first();
-        return view('hello.show', ['item' => $item]);
+        $page = $request->page;
+        $items = DB::table('people')
+        ->offset($page * 3)
+        ->limit(3)
+        ->get();
+        return view('hello.show', ['items' => $items]);
     }
 }
